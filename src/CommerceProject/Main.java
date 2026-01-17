bcedee3d70058a5c758d123a27fe134664ad5310
최길중 <choil8228@gmail.com>
2026-01-17 18:33:23 +0900

    상품 목록 출력 포맷 수정

--- src/CommerceProject/Main.java
+++ src/CommerceProject/Main.java
@@ -1,2 +1,28 @@
-package CommerceProject;public class Main {
+package CommerceProject;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            ArrayList<Product> products = new ArrayList<>();
+
+            System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
+            products.add(new Product(1,"Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 40));
+            products.add(new Product(2,"iPhone 16", 1350000, "Apple의 최신 스마트폰", 50));
+            products.add(new Product(3,"MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 10));
+            products.add(new Product(4,"AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 70));
+
+            // 상품 목록 출력
+            for (Product p : products) {System.out.printf( "%d. | %-14s | %,9d | %-17s | %d%n",p.getId(), p.getProductName(), p.getPrice(), p.getDescription(), p.getInventory());}
+            System.out.printf("%d. | %-14s%n", 0, "프로그램 종료");
+            System.out.print("입력 쿠다사이: ");
+            int input = sc.nextInt();
+            if (input == 0) {break;}
+            System.out.println();
+        }
+    }
 }
--- src/CommerceProject/Product.java
+++ src/CommerceProject/Product.java
@@ -1,2 +1,26 @@
-package CommerceProject;public class Product {
+package CommerceProject;
+
+public class Product {
+    private int id;
+    private String productName; // 제품명
+    private int price; // 가격
+    private String description; // 설명
+    private int inventory; // 재고
+
+    // 생성자 초기화
+    public Product(int id, String productName, int price, String description, int inventory) {
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+        this.description = description;
+        this.inventory = inventory;
+    }
+
+    // 게터 모음
+    public int getId() {return id;}
+    public String getProductName() {return productName;}
+    public int getPrice() {return price;}
+    public String getDescription() {return description;}
+    public int getInventory() {return inventory;}
+
 }
