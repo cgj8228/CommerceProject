54c70381ebd0eea851f104d84d7864520cff283f
최길중 <choil8228@gmail.com>
2026-01-22 19:38:49 +0900

    main 제품추가

--- src/CommerceProject/Main.java
+++ src/CommerceProject/Main.java
@@ -15,12 +15,18 @@ public class Main {
         electronics.addProduct(new Product(4,"AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 70));
         categories.add(electronics);
 
-        Category clothes = new Category(2, "의류");
-        clothes.addProduct(new Product(1, "후드티", 59000, "겨울용 기모 후드", 30));
-        categories.add(clothes);
+        Category runningShoes = new Category(2, "러닝화");
+        runningShoes.addProduct(new Product(1, "아디다스 아디제로 EVO SL", 135000, "아디다스 러닝화", 40));
+        runningShoes.addProduct(new Product(2, "아식스 노바블라스트 5", 159000, "아식스 러닝화", 24));
+        runningShoes.addProduct(new Product(3, "나이키 줌 플라이 6", 137000, "나이키 러닝화", 60));
+        runningShoes.addProduct(new Product(4, "뉴발란스 퓨어셀 슈퍼컴프 엘리트V4", 274000, "뉴발란스 러닝화", 50));
+        categories.add(runningShoes);
 
         Category food = new Category(3, "식품");
         food.addProduct(new Product(1, "프로틴바", 2500, "단백질 간식", 200));
+        food.addProduct(new Product(2, "스테비아 대추방울토마토 2Kg", 23900, "달콤한 토마토", 12));
+        food.addProduct(new Product(3, "펩시 제로슈거 라임향1.5L", 1900, "제로 칼로리 탄산음료", 1000));
+        food.addProduct(new Product(4, "라라스윗 초콜릿 아이스크림474ml", 8290, "저칼로리 아이스크림", 120));
         categories.add(food);
 
         CommerceSystem commerceSystem = new CommerceSystem(categories);
