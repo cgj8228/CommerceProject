4a05757ded90def8858f4c742ee23e544aa7c058
최길중 <choil8228@gmail.com>
2026-01-22 19:44:20 +0900

    등급 시스템 적용(할인율)

--- src/CommerceProject/Customer.java
+++ src/CommerceProject/Customer.java
@@ -9,22 +9,32 @@ public class Customer {
     private String rating;
     private int totalAmount;
 
-    public Customer(String customerName, String email, String rating, int totalAmount) {
+    public Customer(String customerName, String email, int totalAmount) {
         this.customerName = customerName;
         this.email = email;
-        this.rating = rating;
         this.totalAmount = totalAmount;
     }
 
-    public String getRating(String rating) {
-        int t = totalAmount;
-        if (t < 500_000) return "브론즈";
-        if (t < 1_000_000) return "실버";
-        if (t < 2_000_000) return "골드";
+    public String getRating() {
+        if (totalAmount < 500_000) return "브론즈";
+        if (totalAmount < 1_000_000) return "실버";
+        if (totalAmount < 2_000_000) return "골드";
         return "플레티넘";
     }
 
     public void addOrderAmount(int amount) {
         totalAmount += amount;
     }
+
+    public int getTotalAmount() {
+        return totalAmount;
+    }
+
+    public double getDiscountRate() {
+        if (totalAmount < 500_000) return 0.00;
+        if (totalAmount < 1_000_000) return 0.05;
+        if (totalAmount < 2_000_000) return 0.10;
+        return 0.15;
+    }
+
 }
